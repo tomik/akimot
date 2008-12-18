b7161fb0f6c9b9efca54faef79ce66716c5091f4
tomik <tomik@6bcf84ba-f5f3-4bf5-b896-c696636b5bce>
2008-12-18 17:10:29 +0000

    tournament refactored - now using configuration file
    git-svn-id: svn+ssh://zene.sk/home/tomik/svn/akimot/trunk@129 6bcf84ba-f5f3-4bf5-b896-c696636b5bce

--- other/match/tournament.py
+++ other/match/tournament.py
@@ -1,34 +1,56 @@
 import os
+import sys
 import time
 
 bot_1 = "akimot"
 bot_2 = "sample1"
-matches = 10
 match_dir = "matches"
 record_file = "record"
 log_file = "log"
 
-try:
-    os.mkdir(match_dir)
-except: 
-    pass
+from ConfigParser import SafeConfigParser
 
-timestamp = time.strftime("%m%d%H%M%S")
-dir = "%s/%s" % (match_dir, timestamp)
-os.mkdir(dir)
+def runTournament(config):
 
-bot_1_win = 0
+    try:
+        bot_1 =  config.get('global','bot_1')
+        bot_2 =  config.get('global','bot_2')
+        matches = config.getint('global','matches')
+    except:
+        print "Wrong configuration."
+        sys.exit(1)
 
-for i in xrange(matches):
-    print "Match %d/%d" % (i+1, matches)
-    record = "%s/%s_%d" % (dir, record_file, i+1)
-    log = "%s/%s_%d" % (dir, log_file, i)
-    cmdline = "./match %s %s 1>%s 2>%s" % (bot_1, bot_2, record, log)
-    if os.system(cmdline) == 2:
-        print "\nInterrupted ..."
-        break
-    lines = open(record,"r").readlines()
-    if lines[-1][0] == 'w': 
-        bot_1_win += 1 
+    try:
+        os.mkdir(match_dir)
+    except: 
+        pass
 
-print "%d/%d" % (bot_1_win, matches)
+    timestamp = time.strftime("%m%d%H%M%S")
+    dir = "%s/%s" % (match_dir, timestamp)
+    os.mkdir(dir)
+
+    bot_1_win = 0
+
+    for i in xrange(matches):
+        print "Match %d/%d" % (i+1, matches)
+        record = "%s/%s_%d" % (dir, record_file, i+1)
+        log = "%s/%s_%d" % (dir, log_file, i)
+        cmdline = "./match %s %s 1>%s 2>%s" % (bot_1, bot_2, record, log)
+        if os.system(cmdline) == 2:
+            print "\nInterrupted ..."
+            break
+        lines = open(record,"r").readlines()
+        if lines[-1][0] == 'w': 
+            bot_1_win += 1 
+
+    print "%d/%d" % (bot_1_win, matches)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "usage: %s config" %sys.argv[0]
+        sys.exit(1)
+    
+    config = SafeConfigParser()
+    config.readfp(open(sys.argv[1],"r"))
+    runTournament(config)
+    
